b6fe8214e834675533e85af3b1e13ac4ea76deb0
eugeniosaintemarie <eugenio.saintemarie@gmail.com>
2024-03-18 20:17:25 -0300

    Update app.py

--- app.py
+++ app.py
@@ -3,63 +3,27 @@ from bs4 import BeautifulSoup
 import datetime
 import pytz
 
-color_reset = "\033[0m"
-color_rojo = "\033[91m"
-color_naranja = "\033[38;5;208m"
-color_azul = "\033[94m"
-color_celeste = "\033[96m"
-color_amarillo = "\033[93m"
-color_verde = "\033[92m"
-
 
 def simular_publicacion_ficticia():
-    nombre_publicacion = "Producto Ficticio"
-
-    precio_actual = 100000
-    precio_anterior = 200000
-
-    return nombre_publicacion, precio_actual, precio_anterior
+    return "Producto Ficticio", 100000, 200000
 
 
 def obtener_nombre_y_precio(link):
     response = requests.get(link)
-
     if response.status_code != 200:
-        print(color_rojo + f"No se pudo acceder al link {link}" + color_reset)
-        return None
+        return None, None
 
     soup = BeautifulSoup(response.text, "html.parser")
+    nombre_element = soup.find(class_="ui-pdp-title")
+    precio_element = soup.find("span", class_="andes-money-amount__fraction")
 
-    class_id_nombre = "ui-pdp-title"
-    nombre_element = soup.find(class_=class_id_nombre)
+    if not nombre_element or not precio_element:
+        return None, None
 
-    precio_container = soup.find("div", class_="ui-pdp-price__second-line")
-    if precio_container:
-        class_id_precio = "andes-money-amount__fraction"
-        precio_element = precio_container.find("span", class_=class_id_precio)
-    else:
-        precio_element = None
-
-    if precio_element:
-        precio_actual = precio_element.get_text().strip()
-        nombre_publicacion = (
-            nombre_element.get_text().strip()
-            if nombre_element
-            else color_rojo
-            + f"Nombre de publicacion no encontrado {link}"
-            + color_reset
-        )
-        return nombre_publicacion, precio_actual
-    else:
-        print(
-            color_rojo
-            + f"Precio de publicacion no encontrado {nombre_publicacion}"
-            + color_reset
-        )
-        return None
+    return nombre_element.get_text().strip(), float(precio_element.get_text().strip())
 
 
-def generar_html(resultados, enlaces):
+def generar_html(resultados):
     html_content = """
     <!DOCTYPE html>
     <html>
@@ -80,18 +44,12 @@ def generar_html(resultados, enlaces):
     <body>
     """
 
-    for index, resultado in enumerate(resultados, start=1):
-        nombre_publicacion, precio_nuevo, precio_anterior = resultado
-        nombre_publicacion = (
-            (nombre_publicacion[:50] + "...")
-            if len(nombre_publicacion) > 50
-            else nombre_publicacion
-        )
-        nombre_publicacion_link = f'<a href="{enlaces[index-1]}" target="_blank" class="nombre">{nombre_publicacion}</a>'
+    for nombre_publicacion, precio_nuevo, precio_anterior, enlace in resultados:
+        nombre_publicacion_link = f'<a href="{enlace}" target="_blank" class="nombre">{nombre_publicacion}</a>'
         if precio_anterior:
             html_content += f"""
             <div class="item">
-                <div class="nombre">{nombre_publicacion_link}</div>
+                <div>{nombre_publicacion_link}</div>
                 <div class="precio">Precio actual: ${precio_nuevo}</div>
                 <div class="precio_anterior">Precio anterior: ${precio_anterior}</div>
             </div>
@@ -99,7 +57,7 @@ def generar_html(resultados, enlaces):
         else:
             html_content += f"""
             <div class="item">
-                <div class="nombre">{nombre_publicacion_link}</div>
+                <div>{nombre_publicacion_link}</div>
                 <div class="precio">Precio actual: ${precio_nuevo}</div>
             </div>
             """
@@ -120,11 +78,9 @@ def generar_html(resultados, enlaces):
 
 def main():
     mostrar_publicacion_ficticia = True
-
-    if mostrar_publicacion_ficticia:
-        publicacion_ficticia = simular_publicacion_ficticia()
-    else:
-        publicacion_ficticia = None
+    publicacion_ficticia = (
+        simular_publicacion_ficticia() if mostrar_publicacion_ficticia else None
+    )
 
     with open("links.txt", "r") as file:
         enlaces = [line.strip() for line in file]
@@ -140,9 +96,8 @@ def main():
         )
 
     for enlace in enlaces:
-        resultado_obtencion = obtener_nombre_y_precio(enlace)
-        if resultado_obtencion:
-            nombre_publicacion, precio_nuevo = resultado_obtencion
+        nombre_publicacion, precio_nuevo = obtener_nombre_y_precio(enlace)
+        if nombre_publicacion and precio_nuevo:
             nombre_publicacion = nombre_publicacion[:32] + "..."
             if enlace not in precios_guardados:
                 precios_guardados[enlace] = {
@@ -164,7 +119,7 @@ def main():
                     )
                 )
 
-    html_content = generar_html(resultados, enlaces)
+    html_content = generar_html(resultados)
     with open("index.html", "w", encoding="utf-8") as html_file:
         html_file.write(html_content)
 
