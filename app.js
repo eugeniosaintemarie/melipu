dbf2460db2160de5334e82cdad5c622f6a514046
eugeniosaintemarie <eugenio.saintemarie@gmail.com>
2024-03-22 18:58:17 -0300

    Update app.js

--- app.js
+++ app.js
@@ -68,3 +68,27 @@ if (isIOS() && isSafari()) {
 Notification.requestPermission().then(function (result) {
     console.log(result);
 });
+
+navigator.serviceWorker.ready.then(function (registration) {
+    registration.pushManager.subscribe({
+        userVisibleOnly: true,
+        applicationServerKey: urlBase64ToUint8Array('YOUR_PUBLIC_VAPID_KEY_HERE')
+    }).then(function (subscription) {
+        console.log('User is subscribed:', subscription);
+    }).catch(function (error) {
+        console.error('Failed to subscribe the user: ', error);
+    });
+});
+
+function urlBase64ToUint8Array(base64String) {
+    const padding = '='.repeat((4 - base64String.length % 4) % 4);
+    const base64 = (base64String + padding)
+        .replace(/\-/g, '+')
+        .replace(/_/g, '/');
+    const rawData = window.atob(base64);
+    const outputArray = new Uint8Array(rawData.length);
+    for (let i = 0; i < rawData.length; ++i) {
+        outputArray[i] = rawData.charCodeAt(i);
+    }
+    return outputArray;
+}
